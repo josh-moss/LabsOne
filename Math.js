ce5fcab0ca040a07e22b97aeb2e98e9740768c4b
josh <josh-moss@live.nmit.ac.nz>
2020-03-03 14:23:21 +1300

    Updated logs

---
+++ Math.js
@@ -0,0 +1,21 @@
+//week2 Cauculating Variables to Obtain an outcome.
+var numOne
+var numTwo
+var numThree
+var numFour
+var sum 
+
+numOne = 100
+numTwo = 200
+numThree = 300
+numFour = 400
+
+sum = (numOne +(numTwo - numThree)) //100 + (200 - 300)
+//the expected outcome should = 0
+
+console.log("Sum = " + sum)
+
+/* today in sdv503 we learnt how to code simple calculations using JavaScript to obtain the conclusion.
+ Some of the obstacles i had to overcome was accidentaly hard-coding the numbers and outcomes 
+ using an console logged message to display the full calculation. With help from Ali and classmates
+ i was able to understand what i had done wrong and was able to correct my mistakes. */
